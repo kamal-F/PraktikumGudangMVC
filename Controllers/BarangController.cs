1119531f8de76ac576721c77e898a51dde9777ab
kamal-F <kamalonnetx@gmail.com>
2025-07-05 16:48:53 +0700

    Selesai Bab 10: CRUD Lengkap dan Validasi ViewModel

--- Controllers/BarangController.cs
+++ Controllers/BarangController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using GudangWebApp.Models;
+﻿using GudangWebApp.Models;
+using GudangWebApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GudangWebApp.Controllers
 {
@@ -21,15 +22,61 @@ namespace GudangWebApp.Controllers
         public IActionResult Create() => View();
 
         [HttpPost]
-        public IActionResult Create(Barang barang)
+        public IActionResult Create(BarangViewModel vm)
         {
             if (ModelState.IsValid)
             {
+                var barang = new Barang
+                {
+                    KodeBarang = vm.KodeBarang,
+                    NamaBarang = vm.NamaBarang,
+                    JumlahStok = vm.JumlahStok,
+                    Kategori = vm.Kategori
+                };
+
                 _context.Barang.Add(barang);
                 _context.SaveChanges();
                 return RedirectToAction("Index");
             }
+
+            return View(vm);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var barang = _context.Barang.Find(id);
+            if (barang == null) return NotFound();
             return View(barang);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Barang barang)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Barang.Update(barang);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(barang);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var barang = _context.Barang.Find(id);
+            if (barang == null) return NotFound();
+            return View(barang);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var barang = _context.Barang.Find(id);
+            if (barang == null) return NotFound();
+
+            _context.Barang.Remove(barang);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ ViewModels/BarangViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GudangWebApp.ViewModels
+{
+    public class BarangViewModel
+    {
+        [Required]
+        [Display(Name = "Kode Barang")]
+        public string KodeBarang { get; set; }
+
+        [Required]
+        [Display(Name = "Nama Barang")]
+        public string NamaBarang { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Stok tidak boleh negatif")]
+        [Display(Name = "Jumlah Stok")]
+        public int JumlahStok { get; set; }
+
+        [Display(Name = "Kategori")]
+        public string Kategori { get; set; }
+    }
+}
