8dc35af7b9565aa9a2a84660d8bdb2799c0afb53
kamal-F <kamalonnetx@gmail.com>
2025-07-05 13:07:09 +0700

    Add project files.

---
+++ Controllers/BarangController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GudangWebApp.Controllers
+{
+    public class BarangController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
